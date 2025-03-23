87ed3e82237bb50cf0313d3fa7510d97e942be69
RowdyByte <rowdy.verbeek@outlook.com>
2025-03-23 07:26:19 +0100

    Add Capacitor configuration, web manifest, and new favicon assets

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.serialmapper.app',
+  appName: 'serialmapper-prisma',
+  webDir: 'dist'
+};
+
+export default config;

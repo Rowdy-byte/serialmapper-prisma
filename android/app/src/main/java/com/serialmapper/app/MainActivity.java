595090331a79a62c6e97c866142351d2d6c4dd5c
RowdyByte <rowdy.verbeek@outlook.com>
2025-03-23 07:43:40 +0100

    Add Android project structure and initial configuration for Capacitor integration

---
+++ android/app/src/main/java/com/serialmapper/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.serialmapper.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 
@@ -8,7 +8,15 @@ const config = {
 
 	kit: {
 
-		adapter: adapter()
+		adapter: adapter({
+			fallback: 'index.html',
+			strict: false,
+		}),
+		prerender: {
+			entries: [
+
+			],
+		},
 	}
 };
 

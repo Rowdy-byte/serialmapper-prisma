d5cd807b81b4207ac83e331b4e2d3bba79258a62
RowdyByte <rowdy.verbeek@outlook.com>
2025-04-22 08:19:14 +0200

    Refactor HamburgerMenu; adjust layout and restore Docs link

--- src/lib/components/navigation/HamburgerMenu.svelte
+++ src/lib/components/navigation/HamburgerMenu.svelte
@@ -11,8 +11,7 @@
 		{ linkName: 'Inbounds', linkPath: '/inbounds' },
 		{ linkName: 'Outbounds', linkPath: '/outbounds' },
 		{ linkName: 'Products', linkPath: '/products' },
-		{ linkName: 'Clients', linkPath: '/clients' },
-		{ linkName: 'Docs', linkPath: '/docs' }
+		{ linkName: 'Clients', linkPath: '/clients' }
 	];
 
 	function toggle() {
@@ -47,7 +46,7 @@
 	onclick={toggle}
 	aria-label="Toggle navigation menu"
 >
-	MENU
+	Menu
 </button>
 
 <!-- <Hamburger bind:open /> -->
@@ -56,9 +55,9 @@
 	<nav
 		use:clickOutside
 		transition:fade={{ duration: 200 }}
-		class=" absolute top-0 left-0 z-10 flex h-96 w-full items-center justify-center border-b-1 border-slate-500 bg-gray-950 text-white"
+		class=" absolute top-0 left-0 z-10 flex h-96 w-full flex-col items-center justify-center border-b-1 border-slate-500 bg-gray-950 text-white"
 	>
-		<div class="flex flex-col gap-1 p-4 text-start text-xl font-bold">
+		<div class="flex flex-col gap-1 p-4 text-center text-xl font-bold">
 			{#each links as { linkName, linkPath }}
 				{#if pathname !== linkPath}
 					<a onclick={toggle} class="transition-all hover:text-blue-500" href={linkPath}
@@ -74,6 +73,13 @@
 					>
 				{/if}
 			{/each}
+		</div>
+		<div class="flex flex-col gap-1 p-4 text-start text-xl font-bold">
+			<a
+				href="/docs"
+				class=" rounded-lg border border-orange-500 px-4 py-2 font-bold text-orange-500 hover:bg-orange-500 hover:text-white"
+				>Docs</a
+			>
 		</div>
 	</nav>
 {/if}
